8036720b85ff457221b363174f24d5c98668c7ca
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-22 15:30:36 -0800

    added global 404 error handling

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.get("/about", (req, res) => res.render("about"));
 const projects = require("./routes/projects");
 app.use("/projects", projects);
 
+app.use((req, res, next) => {
+  const err = new Error("Sorry, the page you're looking for doesn't exist.");
+  err.status = 404;
+  next(err);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server is istening on port ${port}`));
