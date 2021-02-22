94ee1e0b81b2bf8c31516e31803fb66b7c12e0d1
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-22 14:35:11 -0800

    added static files and index route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+app.set("view engine", "pug");
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => res.render("index"));
 
 const port = process.env.PORT || 3000;
 
