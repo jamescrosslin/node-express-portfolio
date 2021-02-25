ab41eccaeba87fa6359cc2d57bd6ee882e92bd51
James Crosslin <jamesmscrosslin@gmail.com>
2021-02-24 17:59:07 -0800

    added data to main app

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const path = require("path");
+const projects = require("./data.json");
 
 const app = express();
 
 app.set("view engine", "pug");
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index", projects));
 app.get("/about", (req, res) => res.render("about"));
 
-const projects = require("./routes/projects");
-app.use("/projects", projects);
+const projectsRoute = require("./routes/projects");
+app.use("/projects", projectsRoute);
 
 app.use((req, res, next) => {
   const err = new Error("Sorry, the page you're looking for doesn't exist");
