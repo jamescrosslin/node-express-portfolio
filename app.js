2a039cac001d5f21d8355041c07e50fc2613fff6
James Crosslin <jamesmscrosslin@gmail.com>
2021-03-09 10:38:09 -0800

    added styling and error page deets

--- app.js
+++ app.js
@@ -7,19 +7,27 @@ const app = express();
 app.set("view engine", "pug");
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index", projects));
+app.get("/", (req, res) => {
+  throw new Error()
+});
 app.get("/about", (req, res) => res.render("about"));
 
+//project data
 const projectsRoute = require("./routes/projects");
 app.use("/projects", projectsRoute);
 
+
+// 404 error handler
 app.use((req, res, next) => {
   const err = new Error("Sorry, the page you're looking for doesn't exist");
   err.status = 404;
   next(err);
 });
 
+
+// global error handler
 app.use((err, req, res, next) => {
+  // uses a message that exists already on the error object or offers an alternative for untracked internal errors
   const message = err.message || "Internal server error";
   const status = err.status || 500;
   res.status(status);
@@ -28,6 +36,6 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-const port = process.env.PORT || 3000;
-
+// setting the port with this conditional lets me host on Heroku and repl.it
+let port = process.env.PORT ?? 3000;
 app.listen(port, () => console.log(`Server is istening on port ${port}`));
